c344b972389d70a03ae34de06a48c49d569f8d66
Ankita Vaswani <ankita.vaswani@hereon.de>
2025-06-03 00:48:27 +0200

    add read_tsv, write_tsv to __init__.py

--- src/pyecotaxa/__init__.py
+++ src/pyecotaxa/__init__.py
@@ -1,7 +1,7 @@
 from . import _version
 from .remote import Transport, Remote, ImportMode
-from .archive import Archive
+from .archive import Archive, read_tsv, write_tsv
 
 __version__ = _version.get_versions()["version"]
 
-__all__ = ['Transport', 'Remote', 'ImportMode', 'Archive']
+__all__ = ['Transport', 'Remote', 'ImportMode', 'Archive', 'read_tsv', 'write_tsv']
