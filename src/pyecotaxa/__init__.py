c9160a7c20d9cb51c6d072a6b3be5e19cd4597ca
Ankita Vaswani <ankita.vaswani@hereon.de>
2025-06-03 00:42:42 +0200

    add Archive to __init__.py

--- src/pyecotaxa/__init__.py
+++ src/pyecotaxa/__init__.py
@@ -1,6 +1,7 @@
 from . import _version
 from .remote import Transport, Remote, ImportMode
+from .archive import Archive
 
 __version__ = _version.get_versions()["version"]
 
-__all__ = ['Transport', 'Remote', 'ImportMode']
+__all__ = ['Transport', 'Remote', 'ImportMode', 'Archive']
