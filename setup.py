4b36d7cbaff9bda0efee3b118487fe4ec157f876
Ankita Vaswani <ankita.vaswani@hereon.de>
2025-06-03 02:11:26 +0200

    feat batch support to create daily archives

--- setup.py
+++ setup.py
@@ -1,62 +1,23 @@
-from setuptools import find_packages, setup
-
-import versioneer
-
-with open("README.md", "r") as fp:
-    LONG_DESCRIPTION = fp.read()
+from setuptools import setup
 
 setup(
     name="pyecotaxa",
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
-    author="Simon-Martin Schroeder",
-    author_email="sms@informatik.uni-kiel.de",
-    description="Query EcoTaxa and process its output",
-    long_description=LONG_DESCRIPTION,
-    long_description_content_type="text/markdown",
+    version="0.1.0",
+    description="Python client for EcoTaxa",
+    author="EcoTaxa Team",
+    author_email="ecotaxa@obs-vlfr.fr",
     url="https://github.com/ecotaxa/pyecotaxa",
-    packages=find_packages("src"),
     package_dir={"": "src"},
-    include_package_data=True,
+    packages=["pyecotaxa"],
     install_requires=[
-        "pandas>=1.2",
-        "numpy",
-        "python-dotenv",
+        "pandas",
         "requests",
+        "requests-toolbelt",
+        "semantic-version",
         "tqdm",
+        "urllib3",
         "werkzeug",
-        "semantic_version",
         "atomicwrites",
-        "click",
-        "anytree",
-        "python-dateutil",
-        "requests_toolbelt",
     ],
-    python_requires=">=3.6",
-    extras_require={
-        "tests": [
-            # Pytest
-            "pytest",
-            "pytest-cov",
-            "ruff",
-        ],
-        "docs": [
-            "sphinx >= 1.4",
-            "sphinx_rtd_theme",
-            "sphinx-autodoc-typehints>=1.10.0",
-        ],
-        "dev": ["black", "versioneer"],
-    },
-    entry_points={"console_scripts": ["pyecotaxa=pyecotaxa.cli:cli"]},
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Operating System :: OS Independent",
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Science/Research",
-    ],
+    python_requires=">=3.9",
 )
